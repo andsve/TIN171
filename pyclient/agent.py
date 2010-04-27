0e49d6d68b061b5ae4698c883e00e716d33ffcdb
andsve <sweetfish@gmail.com>
2010-04-27 15:45:16 +0200

    dice_props is now global instead.

--- pyclient/agent.py
+++ pyclient/agent.py
@@ -1,6 +1,19 @@
 from messages import *
 from utils import cprint
 
+dice_props = {}
+dice_props[2]  = 0.0278
+dice_props[3]  = 0.0556
+dice_props[4]  = 0.0833
+dice_props[5]  = 0.1111
+dice_props[6]  = 0.1389
+dice_props[7]  = 0.1667
+dice_props[8]  = 0.1389
+dice_props[9]  = 0.1111
+dice_props[10] = 0.0833
+dice_props[11] = 0.0556
+dice_props[12] = 0.0278
+
 class Agent:
     def __init__(self, nickname, gamename, game, client):
         self.gamestate = 0 # 0 = not started, 1 = setup (settle placements), 2 = game running
@@ -17,14 +30,15 @@ class Agent:
 	#
     # Auxiliary gameboard functions
     #
+    
     def calculate_new_settlement_weight(self, node, _round): # add the round number as parameter?
-        weight = weight + resource_weight(node.t1.resource,_round) * dice_probability(node.t1.number)
-        weight = weight + resource_weight(node.t2.resource,_round) * dice_probability(node.t2.number)
-        weight = weight + resource_weight(node.t3.resource,_round) * dice_probability(node.t3.number) 
+        weight = weight + resource_weight(node.t1.resource,_round) * dice_props[node.t1.number]
+        weight = weight + resource_weight(node.t2.resource,_round) * dice_props[node.t2.number]
+        weight = weight + resource_weight(node.t3.resource,_round) * dice_props[node.t3.number]
         return 0
 
     def resource_weight(self, _type, _round)
-        resource_list #make a list of exist resource
+        resource_list = #make a list of exist resource
         if (_round!=1):
             if (!resource_list[_type]):
                 return 40
@@ -33,21 +47,6 @@ class Agent:
                 case 1: #return the weight value of each kind of resource
         
     
-    def dice_probability(self, number):
-        dice_props = {}
-        dice_props[2]  = 0.0278
-        dice_props[3]  = 0.0556
-        dice_props[4]  = 0.0833
-        dice_props[5]  = 0.1111
-        dice_props[6]  = 0.1389
-        dice_props[7]  = 0.1667
-        dice_props[8]  = 0.1389
-        dice_props[9]  = 0.1111
-        dice_props[10] = 0.0833
-        dice_props[11] = 0.0556
-        dice_props[12] = 0.0278
-        return dice_props[number]
-        
     
     def find_buildable_node(self):
         # Returns the best buildable new node!
