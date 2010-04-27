1bc42c487a93b171ee0e216276b5d02b022e61fb
andsve <sweetfish@gmail.com>
2010-04-27 14:52:23 +0200

    Time to add some calculations...

--- pyclient/agent.py
+++ pyclient/agent.py
@@ -17,14 +17,70 @@ class Agent:
 	#
     # Auxiliary gameboard functions
     #
-    def find_buildable_nodes(self):
-        # Returns a list of available nodes
-        # should then be sorted in some way
-        # according to the resources at each node
+    def calculate_new_settlement_weight(self, node):
+    
+        return 0
+    
+    def find_buildable_node(self):
+        # Returns the best buildable new node!
         
         # First, get all buildable nodes!
+        good_nodes = []
         for (k,n) in self.game.buildableNodes.nodes.items():
-            self.debug_print("id: {0} has value {1}".format(k, n))
+            if (n):
+                self.debug_print("Trying see if build is possible on {0}".format(hex(k)))
+                #self.debug_print("Sure build here man!")
+                
+                # look up node in gameboard
+                node = self.game.boardLayout.nodes[k]
+                
+                good_nodes.append({'id': k, 'w': self.calculate_new_settlement_weight(node)})
+                
+                # print neighbour roads
+                #self.debug_print("It has neighbours: {0}, {1}, {2}".format(node.n1, node.n2, node.n3))
+                """
+                # get node neighbours
+                n1 = None
+                if (node.n1 != None):
+                    if (self.game.boardLayout.roads[node.n1].n1 == k):
+                        n1 = self.game.boardLayout.roads[node.n1].n2
+                    elif (self.game.boardLayout.roads[node.n1].n2 == k):
+                        n1 = self.game.boardLayout.roads[node.n1].n1
+                    
+                n2 = None
+                if (node.n2 != None):
+                    if (self.game.boardLayout.roads[node.n2].n1 == k):
+                        n2 = self.game.boardLayout.roads[node.n2].n2
+                    elif (self.game.boardLayout.roads[node.n2].n2 == k):
+                        n2 = self.game.boardLayout.roads[node.n2].n1
+                
+                n3 = None
+                if (node.n3 != None):
+                    if (self.game.boardLayout.roads[node.n3].n1 == k):
+                        n1 = self.game.boardLayout.roads[node.n3].n2
+                    elif (self.game.boardLayout.roads[node.n3].n2 == k):
+                        n1 = self.game.boardLayout.roads[node.n3].n1
+                
+                # check neighbours of node
+                if ((n1 == None or self.game.buildableNodes.nodes[n1]) and 
+                    (n2 == None or self.game.buildableNodes.nodes[n2]) and
+                    (n3 == None or self.game.buildableNodes.nodes[n3])):
+                    self.debug_print("Sure build here man!")
+                else:
+                    self.debug_print("CANT BUILD HERE MANGE!")
+                """
+                
+        best_id = -1
+        best_w = -1
+        # find best one good nodes
+        for v in good_nodes:
+            if (v['w'] > best_w):
+                best_w = v['w']
+                best_id = v['id']
+        
+        # return the best one
+        return best_id
+        
         #print(self.game.buildableNodes)
         
     # incomming messages from the message handler
@@ -44,8 +100,9 @@ class Agent:
         if self.gamestate == 1:
             # Setup state
             if (name == "TurnMessage" and message.playernum == self.playernum):
+                new_settlement_place = self.find_buildable_node()
                 self.debug_print("We should place our settlements now!")
-                self.find_buildable_nodes()
+                
 				
                 # Try to build at pos 0x23!
                 if (self.can_build_at_node(0x23)):
