6928ffe4bd3b93feda1f555e638525b413f0c0a5
jalgelind <jalgelind@gmail.com>
2010-05-13 14:42:54 +0200

    Change to testrun2, uses numcpu*2 number of processes.

--- pyclient/testrun_2.py
+++ pyclient/testrun_2.py
@@ -9,11 +9,11 @@ import multiprocessing
 G_TIMEOUT = 60 * 3
 
 # Number of total threads at any time
-total_threads = 8
+total_threads = multiprocessing.cpu_count() * 2
 num_simul = multiprocessing.Semaphore(total_threads)
 
 # Total number of games to run
-num_games = 100
+num_games = 30
 
 # Score results are saved in this list
 res = []
